990d83899ba1d1c1bce1f12e1f3d60f1d9d15aff
Shahab5826 <malikgee.shahab@gmail.com>
2024-03-21 21:44:59 +0500

    first

---
+++ first.js
@@ -0,0 +1,126 @@
+// // let a , b;
+
+
+// // a = parseInt( prompt("Enter number 1")); 
+// // console.log(a);
+// // b = parseInt(prompt("Enter number 2")); 
+// // console.log(b);
+
+
+// // if (a>b) {
+// //     console.log("The number 1 is greater then number 2."); 
+// //     alert("The number 1 is greater then number 2.");
+// // }
+// // else if (a==b){
+// //     console.log("The number 1 and number 2 are same."); 
+// //     alert("The number 1 and number 2 are same.");
+// // }
+// //  else if(a<b){
+// //     console.log("The number 1 is less then number 2"); 
+// //     alert("The number 1 is less then number 2");
+// // }
+// // else{
+// //     console.log("wrong format."); 
+// //     alert("wrong format.");
+// // }
+// // const student = {
+// //     fullName :"Shahab Ahmed Malik",
+// //     age :20,
+// //     cgpa : 3.7,
+// //     isPass : true,
+// // };
+
+// // student.Name = "Shahab Ahmed";
+
+// // a = String ( prompt("Enter your Name/fullName here ..."));
+
+// // console.log(a);
+// // let a, b, c;
+
+// // b = "name";
+
+// // c = "fullname";
+
+// // a = prompt("Enter your Name/fullName here ...");
+// // a = a.toLowerCase();
+// // console.log("You Enter Your", a.toLowerCase());
+
+// // if (a == b) {
+// //     console.log("And Your Name Is Shahab."); 
+// //     alert("And Your Name Is Shahab.");
+// // }
+// // else if (a == c){
+// //     console.log("And Your fullName Is Shahab Ahmed Malik."); 
+// //     alert("And Your fullName Is Shahab Ahmed Malik.");
+// // }
+// // else{
+// //     console.log("Wrong Format."); 
+// //     alert("Wrong Format.");
+// // }
+//  let a, b, c, d, e, f, g, h, i, j, k;
+//  a = parseInt(prompt("Enter your First Number."));
+//  console.log("The First Number You Enter Is",a);
+//  c = prompt("Enter your Operator Which You Perform.");
+//  console.log("The Operater You Enter Is",c);
+//  b = parseInt(prompt("Enter your Second Number."));
+//  console.log("The Second Number You Enter Is",b);
+//  k = "+";
+//  d = "-";
+//  e = "*";
+//  f = "/";
+//  g = a + b;
+//  h = a - b;
+//  i = a * b;
+//  j = a / b;
+ 
+// //  if (a == k == "+" == b) {
+// //     console.log("a + b =", g); 
+// //     alert("Your Answer Is", g);
+// // }
+// // else if (a == d == b){
+// //     console.log("a - b =", h); 
+// //     alert("Your Answer Is", h);
+// // }
+// // else if (a == e == b){
+// //     console.log("a * b =", i); 
+// //     alert("Your Answer Is", i);
+// // }
+// // else if (a == f == b){
+// //     console.log("a / b =", j); 
+// //     alert("Your Answer Is", j);
+// // }
+// // else{
+// //     console.log("Wrong Format."); 
+// //     alert("Wrong Format.");
+// // }
+// let num1, num2, opr, result;
+
+// num1 = parseInt(prompt("Enter Your First Number"));
+// console.log("Your First Number Is",num1);
+// num2 = parseInt(prompt("Enter Your Second Number"));
+// console.log("And Your Second Number Is",num2);
+
+// opr = prompt("Press Key A For (adding), S For (Substracting), M For (Multiplication) and D for (division)");
+// opr = opr.toLowerCase();
+
+// if (opr === 'a') {
+//     result = num1 + num2
+//     console.log("a + b =", num1, "+", num2, "=", result);
+//     alert(`Your Result Is ${result}`)
+// } else if (opr === 's') {
+//     result = num1 - num2
+//     console.log("a - b =", num1, "-", num2, "=", result);
+//     alert(`Your Result Is ${result}`)
+// } else if (opr === 'm') {
+//     result = num1 * num2
+//     console.log("a * b =", num1, "*", num2, "=", result);
+//     alert(`Your Result Is ${result}`)
+// } else if (opr === 'd') {
+//     result = num1 / num2
+//     console.log("a / b =", num1, "/", num2, "=", result);
+//     alert(`Your Result Is ${result}`);
+// }
+// else{
+//         console.log("Wrong Format."); 
+//         alert("Wrong Format.");
+//     }
